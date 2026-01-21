a579096e8eea05792016c3331549bb32fc3ca299
Ramil Llanes <ramil01llanes124@gmail.com>
2026-01-21 21:10:34 +0800

    Agent Function Profile

--- Agent/Agent.js
+++ Agent/Agent.js
@@ -3,23 +3,14 @@ function Agent_Profile() {
     const Profile = document.querySelector('.Division-Portfollio');
     const Division = document.querySelector('.main-section');
     const Featured = document.querySelector('.Agent-Promotion');
-    const ViewMore = document.getElementById('ViewMore');
+    const ViewMore = document.querySelectorAll('.btn');    
 
-    // ViewMore.addEventListener('click', () => {
-
-    //     Profile.style.display = 'block';
-
-    //     Division.style.display = 'none';
-
-    //     Featured.style.display = 'none';
-    // })
-    
-
-    
 
     ViewMore.forEach(button => {
     button.addEventListener('click', () => {
         Profile.style.display = 'block';
+        Division.style.display = 'none';
+        Featured.style.display = 'none';
     });
     });
 
